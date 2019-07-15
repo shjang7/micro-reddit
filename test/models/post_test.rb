2618703a15886fba7cd9fd3e78d419d32345c959
wathigo <wathigosimon@gmail.com>
2019-07-15 06:57:17 -0400

    Add the tests for the post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:sam)
+    @post = Post.new(title: "Title", content: "Example content", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = "     "
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "     "
+    assert_not @post.valid?
+  end
 end
