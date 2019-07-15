fccda6660d94ebccfb4503cb6a8cb1c62991cf83
wathigo <wathigosimon@gmail.com>
2019-07-15 06:58:47 -0400

    Add tests for the comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:sam)
+    @post = posts(:one)
+    @comment = Comment.new(content: "some comment", post_id: @post.id, user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test "Content field should be present" do
+    @comment.content = "     "
+    assert_not @comment.valid?
+  end
 end
